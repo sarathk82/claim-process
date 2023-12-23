70b412c03752e3511801fc11d3b254db4a3b84bd
sarathk82 <84178883+sarathk82@users.noreply.github.com>
2023-12-23 18:24:04 +0530

    Home Page update for horizontal menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,33 +6,56 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './Header.css';
 
 const Header = () => {
-    const [showMenu, setShowMenu] = useState(false);
     const [showProfile, setShowProfile] = useState(false);
-
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-        setShowProfile(false);
-    };
+    const [showProjectsSubMenu, setShowProjectsSubMenu] = useState(false);
+    const [showTeamsSubMenu, setShowTeamsSubMenu] = useState(false);
 
     const toggleProfile = () => {
         setShowProfile(!showProfile);
-        setShowMenu(false);
+    };
+
+    const toggleProjectsSubMenu = () => {
+        setShowProjectsSubMenu(!showProjectsSubMenu);
+        setShowTeamsSubMenu(false); // Hide Teams submenu when Projects submenu is toggled
+    };
+
+    const toggleTeamsSubMenu = () => {
+        setShowTeamsSubMenu(!showTeamsSubMenu);
+        setShowProjectsSubMenu(false); // Hide Projects submenu when Teams submenu is toggled
+    };
+
+    const handleProjectsMouseLeave = () => {
+        setShowProjectsSubMenu(false);
+    };
+
+    const handleTeamsMouseLeave = () => {
+        setShowTeamsSubMenu(false);
     };
 
     return (
         <header>
-            <div className="menu-icon" onClick={toggleMenu}>
-                <i className={`fas fa-bars ${showMenu ? 'active' : ''}`}></i>
-                {showMenu && (
-                    <div className="menu">
-                        <Link to="/create-project">Create Project</Link>
-                        <Link to="/view-projects">View Projects</Link>
-                        <Link to="/create-team">Create Team</Link>
-                        <Link to="/view-teams">View Teams</Link>
-                    </div>
-                )}
+            <div className="menu">
+                <div className="menu-item" onMouseEnter={toggleProjectsSubMenu} onMouseLeave={handleProjectsMouseLeave}>
+                    <Link to="#">Projects</Link>
+                    {showProjectsSubMenu && (
+                        <div className="submenu" onMouseEnter={toggleProjectsSubMenu} onMouseLeave={handleProjectsMouseLeave}>
+                            <Link to="/create-project">Create</Link>
+                            <Link to="/view-projects">View</Link>
+                        </div>
+                    )}
+                </div>
+                <div className="menu-item" onMouseEnter={toggleTeamsSubMenu} onMouseLeave={handleTeamsMouseLeave}>
+                    <Link to="#">Teams</Link>
+                    {showTeamsSubMenu && (
+                        <div className="submenu" onMouseEnter={toggleTeamsSubMenu} onMouseLeave={handleTeamsMouseLeave}>
+                            <Link to="/create-team">Create</Link>
+                            <Link to="/view-teams">View</Link>
+                        </div>
+                    )}
+                </div>
             </div>
-            <div><h3>  Complan Claims Processing </h3></div>
+
+            <div><h3>Claims Processing</h3></div>
             <div className="profile-icon">
                 <i className={`fas fa-user-circle ${showProfile ? 'active' : ''}`} onClick={toggleProfile}></i>
                 {showProfile && (
